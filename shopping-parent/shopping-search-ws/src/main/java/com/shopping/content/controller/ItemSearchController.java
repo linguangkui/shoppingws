eae4e9b06aa940bd3b841562e72f20a9567924bd
linguangkui <linguangkui@163.com>
2020-09-27 16:33:25 +0800

    添加了搜索模块

---
+++ shopping-parent/shopping-search-ws/src/main/java/com/shopping/content/controller/ItemSearchController.java
@@ -0,0 +1,20 @@
+package com.shopping.content.controller;
+
+import com.shopping.content.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ItemSearchController {
+    @Autowired
+    private ItemSearchService itemSearchService;
+
+    @RequestMapping("/search")
+    public Map search(@RequestBody Map searchMap){
+        return itemSearchService.search(searchMap);
+    }
+}
---
+++ shopping-parent/shopping-search-ws/src/main/java/com/shopping/content/service/impl/ItemSearchServiceImpl.java
@@ -0,0 +1,49 @@
+package com.shopping.content.service.impl;
+
+import com.online.shopping.pojo.TbItem;
+import com.shopping.content.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ItemSearchServiceImpl implements ItemSearchService {
+    @Autowired
+    private SolrTemplate solrTemplate;
+    @Override
+    public Map search(Map searchMap) {
+        Map map = new HashMap();
+
+        Query query = new SimpleQuery("*:*");
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+
+        ScoredPage<TbItem> items = solrTemplate.queryForPage("collection1", query, TbItem.class);
+        map.put("rows",items.getContent());
+        return map;
+    }
+
+    @Override
+    public void importList(List list) {
+        solrTemplate.saveBeans("collection1",list);
+        solrTemplate.commit("collection1");
+    }
+
+    @Override
+    public void deleteByGoodsIds(List goodsIds) {
+        Query query = new SimpleQuery("*:*");
+        Criteria criteria = new Criteria("item_goodsid").in(goodsIds);
+        query.addCriteria(criteria);
+
+        solrTemplate.delete("collection1",query);
+        solrTemplate.commit("collection1");
+    }
+}
