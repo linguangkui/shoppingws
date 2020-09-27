24ca91dcfec778759f580942f643ac67d882d6f6
linguangkui <linguangkui@163.com>
2020-09-27 16:34:39 +0800

    修改了一些细节

--- shopping-parent/shopping-search-ws/src/main/java/com/shopping/content/controller/ItemSearchController.java
+++ shopping-parent/shopping-search-ws/src/main/java/com/shopping/content/controller/ItemSearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/itemsearch-ms")
 public class ItemSearchController {
     @Autowired
     private ItemSearchService itemSearchService;
