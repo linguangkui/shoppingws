b713917d32b1ac28be8ee9d588228452751d87bb
linguangkui <linguangkui@163.com>
2020-09-28 10:25:36 +0800

    修改了RedisConfig

--- shopping-parent/shopping-content-ws/src/main/java/com/shopping/content/redis/RedisConfig.java
+++ shopping-parent/shopping-content-ws/src/main/java/com/shopping/content/redis/RedisConfig.java
@@ -3,21 +3,22 @@ package com.shopping.content.redis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
-@Configuration
+//@Configuration
 public class RedisConfig {
 	
 	/*@Autowired
  	private RedisConnectionFactory redisConnectionFactory;*/
 
-	/*@Bean
+	@Bean
 	public RedisConnectionFactory RedisConnectionFactory() {
 		RedisConnectionFactory factory = new JedisConnectionFactory();
 		return factory;	
-	}*/
+	}
 	
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
